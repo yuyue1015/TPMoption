bcb633ae197fb067dc1f80276838ff9b489c734d
Yueyibai <143266428+yuyue1015@users.noreply.github.com>
2026-01-27 14:54:11 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "双点博物馆档案库",
-  description: "博物馆展品搜索系统",
-};
+  title: '探险困境选择指南',
+  description: '查询探险困境的选择与结果',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="zh-CN">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
