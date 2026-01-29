926e24acbb793dad37b2abc2baaba48e9333f38f
Yueyibai <143266428+yuyue1015@users.noreply.github.com>
2026-01-29 18:16:44 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: false // 强制开启，因为我们没有本地开发环境
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true }
+  images: {
+    unoptimized: true,
+  },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
